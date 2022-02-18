7461c2721c162daade33f588241ada57ee020204
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-18 10:27:11 +0500

    bye bye

--- plugins/user-timezone.js
+++
@@ -1,7 +0,0 @@
-export default ({ app, store }, inject) => {
-  // Inject user timezone in Vue, context and store.
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  inject('timezone', () => timeZone)
-  window.updateUserTimeZone = (timeZone) =>
-    store.commit('updateUserTimeZone', timeZone)
-}
